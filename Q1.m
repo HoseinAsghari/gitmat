2d6319a3c22bf0f55b8f003b84ca9a9835c660c3
HoseinAsghari <109804632+HoseinAsghari@users.noreply.github.com>
2023-07-04 18:17:03 +0330

    Update Q1.m finding coordinates where maximum correlation occurs

--- Q1.m
+++ Q1.m
@@ -6,7 +6,7 @@ t=(imread('template.JPG'));% read template image
 t=imcrop(t,[0,0,tr,tr]);% to fix this image to real size we need to omit some trash pixels 
 R=normxcorr2(t(:,:,3),b(:,:,3));% finding normal cross correlation 
 R=imcrop(R,[tr,tr,ic,ir]);% crop the extra pixels
-[r,c,v]=find(R==max(max(R)));%finding coordinates where maximum correlation occurs
+[r,c,v]=find(R==max(max(R)));% finding coordinates where maximum correlation occurs
 Result=insertShape(b,'Rectangle',[c,r,tr,tr],'LineWidth',5);%create a box around maximum match
 figure(1)
 subplot(2,2,1)
