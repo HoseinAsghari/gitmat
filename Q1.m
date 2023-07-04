f2bde1fdca2486f105a6550a49dcd33993bbe946
HoseinAsghari <109804632+HoseinAsghari@users.noreply.github.com>
2023-07-04 18:16:00 +0330

    Update Q1.m % to fix this image to real size we need to omit some trash pixels

--- Q1.m
+++ Q1.m
@@ -3,7 +3,7 @@ b=(imread('background.JPG'));% read background image
 t=(imread('template.JPG'));% read template image 
 [ir,ic]=size(b);% get size of background
 [tr,tc]=size(t);%get size of template
-t=imcrop(t,[0,0,tr,tr]);% to fix this image to real size we need to omit some trash pixels
+t=imcrop(t,[0,0,tr,tr]);% to fix this image to real size we need to omit some trash pixels 
 R=normxcorr2(t(:,:,3),b(:,:,3));% finding normal cross correlation
 R=imcrop(R,[tr,tr,ic,ir]);% crop the extra pixels
 [r,c,v]=find(R==max(max(R)));%finding coordinates where maximum correlation occurs
