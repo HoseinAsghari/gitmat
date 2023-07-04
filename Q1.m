1b001962e1c09428afc0d050a05a4d1e1b1b300d
HoseinAsghari <109804632+HoseinAsghari@users.noreply.github.com>
2023-07-04 18:17:48 +0330

    Update Q1.m create a box around maximum match

--- Q1.m
+++ Q1.m
@@ -7,7 +7,7 @@ t=imcrop(t,[0,0,tr,tr]);% to fix this image to real size we need to omit some tr
 R=normxcorr2(t(:,:,3),b(:,:,3));% finding normal cross correlation 
 R=imcrop(R,[tr,tr,ic,ir]);% crop the extra pixels
 [r,c,v]=find(R==max(max(R)));% finding coordinates where maximum correlation occurs
-Result=insertShape(b,'Rectangle',[c,r,tr,tr],'LineWidth',5);% create a box around maximum match
+Result=insertShape(b,'Rectangle',[c,r,tr,tr],'LineWidth',5);% create a box around maximum match 
 figure(1)
 subplot(2,2,1)
 imshow(b)
