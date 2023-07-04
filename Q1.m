4157e207d912c91d7658808ca7fe61b71cfc84cc
HoseinAsghari <109804632+HoseinAsghari@users.noreply.github.com>
2023-07-04 18:16:18 +0330

    Update Q1.m finding normal cross correlation

--- Q1.m
+++ Q1.m
@@ -4,7 +4,7 @@ t=(imread('template.JPG'));% read template image
 [ir,ic]=size(b);% get size of background
 [tr,tc]=size(t);%get size of template
 t=imcrop(t,[0,0,tr,tr]);% to fix this image to real size we need to omit some trash pixels 
-R=normxcorr2(t(:,:,3),b(:,:,3));% finding normal cross correlation
+R=normxcorr2(t(:,:,3),b(:,:,3));% finding normal cross correlation 
 R=imcrop(R,[tr,tr,ic,ir]);% crop the extra pixels
 [r,c,v]=find(R==max(max(R)));%finding coordinates where maximum correlation occurs
 Result=insertShape(b,'Rectangle',[c,r,tr,tr],'LineWidth',5);%create a box around maximum match
